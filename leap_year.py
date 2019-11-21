ed0aba2223f6383b565730a2b3bd2e757aec392e
Kaazi <47986340+IsaacColney@users.noreply.github.com>
2019-11-21 22:51:34 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,16 +1,25 @@
-year = int(input("Enter the year :"))
+
+year = input("Enter the year :")
+if year != int():
+    print ('Input integer only!!!!')
+    year = int(input("Enter the year :"))
+else:
+    is_leap(year)
+    
 def is_leap(year):
     pass
     a = "The entered year is Leap Year"
     b = "The entered year is not leap year"
-    if year in range (1900,10**5):
-        if year % 4 == 0:   
-            if year % 100 == 0:
-                if year % 100 == 0 and year % 400 == 0:                 
-                    return a   
-                return b 
-            return a
-        else:
-            return b
+    if year % 4 == 0:   
+        if year % 100 == 0:
+            if year % 100 == 0 and year % 400 == 0:                 
+                return a   
+            return b 
+        return a
+    else:
+        return b
+    
 
 print(is_leap(year))
+
+input("Press any Key to close.......")
