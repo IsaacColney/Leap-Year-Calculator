796cd238a5bf0f9eed194bc223e9e8c35357a036
Kaazi <47986340+IsaacColney@users.noreply.github.com>
2019-12-21 23:17:56 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,25 +1,29 @@
+#Leap_Year
 
-year = input("Enter the year :")
+cont = "y"
 
-if year != int():
-    print ('Input integer only!!!!')
+while cont.lower() == 'y':
+
     year = int(input("Enter the year :"))
-else:
-    is_leap(year)
-    
-def is_leap(year):
-    pass
-    a = "The entered year is Leap Year"
-    b = "The entered year is not leap year"
-    if year % 4 == 0:   
-        if year % 100 == 0:
-            if year % 100 == 0 and year % 400 == 0:                 
-                return a   
-            return b 
-        return a
-    else:
-        return b
-    
-print(is_leap(year))
 
-input("Press any Key to close.......")
+  
+    def is_leap(year):
+        pass
+        a = "The entered year is Leap Year"
+        b = "The entered year is not leap year"
+        if year % 4 == 0:   
+            if year % 100 == 0:
+                if year % 100 == 0 and year % 400 == 0:                 
+                    return a   
+                return b 
+            return a
+        else:
+            return b
+    
+    print(is_leap(year))
+    
+    cont = input("Do you want to check another Year? y/n:")
+    if cont == "n":
+        break
+
+
